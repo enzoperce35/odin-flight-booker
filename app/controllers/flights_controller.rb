0edd21eb350af517d570b3bfee8bd1da5a4a795f
enzoperce35 <enzoperce35@gmail.com>
2022-01-14 16:49:57 +0800

    search form created

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -1,4 +1,27 @@
 class FlightsController < ApplicationController
   def index
+    @airports = Airport.all
+    @airport_options = Airport.all.map { |a| [a.name, a.id] }
+
+    return if flight_params.empty?
+
+    @flights = Flight.where(flight_params)
+    @booking_options = find_booking_choices
+  end
+
+
+  private
+
+  def flight_params
+    params.permit(:origin_id, :destination_id, :departure_date, :departure_time)
+  end
+
+  def find_booking_choices
+    if params[:origin_id] == params[:destination_id]  
+      flash[:notice] = "Origin and destination cannot be the same"
+      render :index
+    else
+      BookingsHelper::BookingChoices.new(flight_params).find_flights
+    end
   end
 end
