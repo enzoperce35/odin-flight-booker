e37167020a64d2406e89ada130f8d032c1741c0d
enzoperce35 <enzoperce35@gmail.com>
2022-01-15 06:51:11 +0800

    set up create action

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,8 +1,22 @@
 class BookingsController < ApplicationController
   def new
+    @booking = Booking.new
+    @booking_choice = Flight.find(params[:booking_option])
+    passenger_count = params[:num_tickets].to_i
+    passenger_count.times { @booking.passengers.build }
   end
 
   def create
+    @booking = Booking.new(passenger_params)
+
+    if @booking.save
+      flash[:notice] = "Booking created successfully."
+      PassengerMailer.confirmation_email(@booking).deliver_now
+      redirect_to @booking
+    else
+      flash[:alert] = "Sorry, there was a problem creating your booking."
+      render :new
+    end
   end
 
   def show
