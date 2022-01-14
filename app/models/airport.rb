a03d09bea1da1bb313474404b635f4890366ec58
enzoperce35 <enzoperce35@gmail.com>
2022-01-14 16:32:02 +0800

    set-up associations

--- app/models/airport.rb
+++ app/models/airport.rb
@@ -1,2 +1,4 @@
 class Airport < ApplicationRecord
+  has_many :departing_flights, class_name: 'Flight', foreign_key: 'origin_id', inverse_of: :to_airport
+  has_many :arriving_flights, class_name: 'Flight', foreign_key: 'destination_id', inverse_of: :from_airport
 end
--- app/models/flight.rb
+++ app/models/flight.rb
@@ -1,2 +1,4 @@
 class Flight < ApplicationRecord
+  belongs_to :from_airport, class_name: 'Airport', foreign_key: 'origin_id', inverse_of: :arriving_flights
+  belongs_to :to_airport, class_name: 'Airport', foreign_key: 'destination_id', inverse_of: :departing_flights
 end
